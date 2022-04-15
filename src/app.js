606781e47f22c4891870adc7ec499c4f31d84826
Nestor David Castillo <nestor.castillo@ieee.org>
2022-04-15 10:47:19 -0500

    Web Layout Responsive: Mobile (<768px), Tablet(min-width:768px), Desktop(min-width:1440px)

--- src/app.js
+++ src/app.js
@@ -1,15 +1,23 @@
 function showpage2(page1,page2){
     var page1 = document.getElementById(page1);
     var page2 = document.getElementById(page2);
+    var button1 = document.getElementById('button1');
+    var button2 = document.getElementById('button2');
     page1.style.display = 'none';
     page2.style.display = 'block';
+    button1.style.backgroundColor = '#D9D9D9';
+    button2.style.backgroundColor = 'violet';
 }
 
 function showpage1(page1,page2){
     var page1 = document.getElementById(page1);
     var page2 = document.getElementById(page2);
+    var button1 = document.getElementById('button1');
+    var button2 = document.getElementById('button2');
     page1.style.display = 'block';
     page2.style.display = 'none';
+    button1.style.backgroundColor = 'violet';
+    button2.style.backgroundColor = '#D9d9d9';
 }
 
 function time(){
